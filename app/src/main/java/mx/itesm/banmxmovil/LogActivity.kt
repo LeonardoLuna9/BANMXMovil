57b2cf90787530be6b85ef9e535e179363e9b7a9
Wulf450 <drvmzo@hotmail.com>
2022-09-25 17:15:02 -0500

    Firebase base

--- app/src/main/java/mx/itesm/banmxmovil/LogActivity.kt
+++ app/src/main/java/mx/itesm/banmxmovil/LogActivity.kt
@@ -1,11 +1,45 @@
 package mx.itesm.banmxmovil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import mx.itesm.banmxmovil.databinding.ActivityLogBinding
 
 class LogActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityLogBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log)
+        //setContentView(R.layout.activity_log)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_log)
+    }
+
+    fun login(view : View?){
+
+        var emailStr = binding.editTextTextEmailLogin.text.toString()
+        var pwdStr = binding.editTextTextPassword.text.toString()
+        var authTask = Firebase.auth.signInWithEmailAndPassword(emailStr, pwdStr)
+
+        authTask.addOnCompleteListener(this) { resultado ->
+
+            if(resultado.isSuccessful){
+
+                Toast.makeText(this, "LOGIN EXITOSO", Toast.LENGTH_SHORT).show()
+            } else {
+
+                Toast.makeText(this, "ERROR EN LOGIN", Toast.LENGTH_SHORT).show()
+                Log.e("FIREBASE-DEV", "error: ${resultado.exception?.message}")
+            }
+        }
+    }
+
+    fun clickTextViewRegister(view: View?) {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
