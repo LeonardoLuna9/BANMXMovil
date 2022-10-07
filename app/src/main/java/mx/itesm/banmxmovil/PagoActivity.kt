f5307c42ce56e2c982ce494a1fc95ad41cebe7e9
PaulinaGtz <paulinagtz16@gmail.com>
2022-10-07 00:09:29 -0500

    Pago

--- app/src/main/java/mx/itesm/banmxmovil/PagoActivity.kt
+++ app/src/main/java/mx/itesm/banmxmovil/PagoActivity.kt
@@ -2,8 +2,17 @@ package mx.itesm.banmxmovil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import mx.itesm.banmxmovil.databinding.ActivityPagoBinding
+
+var tarjeta1:RadioButton? = null
+var tarjeta2:RadioButton? = null
+var tarjeta3:RadioButton? = null
+var radiogroup:RadioGroup? = null
 
 class PagoActivity : AppCompatActivity() {
 
@@ -13,5 +22,19 @@ class PagoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         // setContentView(R.layout.activity_pago)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_pago)
+
+        radiogroup = findViewById(R.id.radioGroup2)
+        tarjeta1 = findViewById(R.id.botonTarjetaCreditoPago)
+        tarjeta2 = findViewById(R.id.botonTarjetaDebitoPago)
+        tarjeta3 = findViewById(R.id.botonPaypalPago)
+        radiogroup?.setOnCheckedChangeListener(this)
+    }
+
+    override fun onCheckedChanged(p0: RadioGroup?, idRadio: Int){
+        when (idRadio) {
+            tarjeta1?.id -> Toast.makeText(this, "Tarjeta Seleccionada", Toast.LENGTH_LONG).show()
+            tarjeta2?.id -> Toast.makeText(this, "Tarjeta Seleccionada", Toast.LENGTH_LONG).show()
+            tarjeta3?.id -> Toast.makeText(this, "Tarjeta Seleccionada", Toast.LENGTH_LONG).show()
+        }
     }
 }
