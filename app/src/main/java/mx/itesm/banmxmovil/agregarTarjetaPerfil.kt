8015ce39332fb369492d75b76d4902e07b6ba359
Wulf450 <drvmzo@hotmail.com>
2022-10-07 00:50:13 -0500

    Ya registramos tarjetas

--- app/src/main/java/mx/itesm/banmxmovil/agregarTarjetaPerfil.kt
+++ app/src/main/java/mx/itesm/banmxmovil/agregarTarjetaPerfil.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.firestore.ktx.firestore
@@ -14,6 +15,11 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
 class agregarTarjetaPerfil : Fragment() {
+
+    private lateinit var _nombreInputTarjeta : EditText
+    private lateinit var _numTarjetaInputTarjeta : EditText
+    private lateinit var _expInputTarjeta : EditText
+    private lateinit var _codigoInputTarjeta : EditText
 
     val db = Firebase.firestore
     //val args : perfilFragmentArgs by navArgs()
@@ -25,19 +31,25 @@ class agregarTarjetaPerfil : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_agregar_tarjeta_perfil, container, false)
 
+        _nombreInputTarjeta = view.findViewById(R.id.nombreInputTarjeta)
+        _numTarjetaInputTarjeta = view.findViewById(R.id.numTarjetaInputTarjeta)
+        _expInputTarjeta = view.findViewById(R.id.expInputTarjeta)
+        _codigoInputTarjeta = view.findViewById(R.id.codigoInputTarjeta)
 
         view.findViewById<Button>(R.id.guardarBotonTarjeta).setOnClickListener{
+            val data = hashMapOf(
+                "nombre" to _nombreInputTarjeta.text.toString(),
+                "numTarjeta" to _numTarjetaInputTarjeta.text.toString(),
+                "expTarjeta" to _expInputTarjeta.text.toString(),
+                "expTarjeta" to _codigoInputTarjeta.text.toString()
+            )
+
+            //view.findViewById<TextView>(R.id.nombreViewPerfil).text = args.nombrePerfil
+            //view.findViewById<TextView>(R.id.correoViewPerfil).text = args.emailPerfil
+            db.collection("usuarios/prueba@mail.com/tarjetas").document(_numTarjetaInputTarjeta.text.toString()).set(data)
+
             findNavController().navigate(R.id.action_agregarTarjetaPerfil_to_misTarjetasPerfilFragment)
         }
-
-        val city = hashMapOf(
-            "name" to "Los Angeles",
-            "state" to "CA",
-            "country" to "USA"
-        )
-        //view.findViewById<TextView>(R.id.nombreViewPerfil).text = args.nombrePerfil
-        //view.findViewById<TextView>(R.id.correoViewPerfil).text = args.emailPerfil
-        db.collection("usuarios/tarjetas").document("aqui").set(city)
 
 
         return view
