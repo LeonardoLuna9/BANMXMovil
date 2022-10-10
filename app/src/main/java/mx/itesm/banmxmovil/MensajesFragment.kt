9a91c69ed414fa9ec42951f956c0deecd05c431d
Juan Pablo Garcia <a01639025@tec.mx>
2022-10-09 23:34:40 -0500

    args update

--- app/src/main/java/mx/itesm/banmxmovil/MensajesFragment.kt
+++ app/src/main/java/mx/itesm/banmxmovil/MensajesFragment.kt
@@ -6,18 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MensajesFragment : Fragment() {
-
+    val args : MensajesFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_mensajes, container, false)
+        if(Firebase.auth.currentUser == null) {
+
+            // SIGNIFICA QUE HAY NECESIDAD DE RE-VALIDAR EL USUARIO
+            // podrías redireccionar / terminar esta actividad
+            Toast.makeText(context, "REVALIDA!", Toast.LENGTH_SHORT).show()
+            requireActivity().finish()
+        }
         view.findViewById<ImageButton>(R.id.regresarBotonApadrinar2).setOnClickListener {
-            findNavController().navigate(R.id.action_mensajesFragment_to_apadrinarFragment)
+            val action = MensajesFragmentDirections
+                .actionMensajesFragmentToApadrinarFragment(
+                    args.idUsuario
+                )
+            findNavController().navigate(action)
         }
         return view
     }
--- app/src/main/java/mx/itesm/banmxmovil/donarFragment.kt
+++ app/src/main/java/mx/itesm/banmxmovil/donarFragment.kt
@@ -6,26 +6,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class donarFragment : Fragment() {
-
+    val args : donarFragmentArgs by navArgs()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_donar, container, false)
+        if(Firebase.auth.currentUser == null) {
+
+            // SIGNIFICA QUE HAY NECESIDAD DE RE-VALIDAR EL USUARIO
+            // podrías redireccionar / terminar esta actividad
+            Toast.makeText(context, "REVALIDA!", Toast.LENGTH_SHORT).show()
+            requireActivity().finish()
+        }
+
         view.findViewById<Button>(R.id.botonDonarBienFragment).setOnClickListener{
-            findNavController().navigate(R.id.action_donarFragment_to_pagoFragment)
+            val action = donarFragmentDirections
+                .actionDonarFragmentToPagoFragment(
+                    args.idUsuario
+                )
+            findNavController().navigate(action)
         }
         view.findViewById<Button>(R.id.configDonar).setOnClickListener{
-            findNavController().navigate(R.id.action_donarFragment_to_perfilFragment)
+            val action = donarFragmentDirections
+                .actionDonarFragmentToPerfilFragment(
+                    args.idUsuario
+                )
+            findNavController().navigate(action)
         }
         view.findViewById<Button>(R.id.apadrinarDonar).setOnClickListener{
-            findNavController().navigate(R.id.action_donarFragment_to_apadrinarFragment)
+            val action = donarFragmentDirections
+                .actionDonarFragmentToApadrinarFragment(
+                    args.idUsuario
+                )
+            findNavController().navigate(action)
         }
         view.findViewById<Button>(R.id.cartDonar).setOnClickListener{
-            findNavController().navigate(R.id.action_donarFragment_to_carritoFragment)
+            val action = donarFragmentDirections
+                .actionDonarFragmentToCarritoFragment(
+                    args.idUsuario
+                )
+            findNavController().navigate(action)
         }
 
 
